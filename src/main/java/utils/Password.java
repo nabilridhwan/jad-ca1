07eedd314354e7aafb32a9014bc96d1e7f070bec
catcd1w3r5 <45095604+catcd1w3r5@users.noreply.github.com>
2022-06-26 20:28:47 +0800

    Update Password.java

--- src/main/java/utils/Password.java
+++ src/main/java/utils/Password.java
@@ -20,15 +20,15 @@ public class Password {
             BigInteger no = new BigInteger(1, messageDigest);
   
             // Convert message digest into hex value
-            String hashtext = no.toString(16);
+            StringBuilder hashtext = new StringBuilder(no.toString(16));
   
             // Add preceding 0s to make it 32 bit
             while (hashtext.length() < 32) {
-                hashtext = "0" + hashtext;
+                hashtext.insert(0, "0");
             }
   
             // return the HashText
-            return hashtext;
+            return hashtext.toString();
         }
   
         // For specifying wrong message digest algorithms
