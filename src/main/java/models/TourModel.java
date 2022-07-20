3009b7257e90cef316870e46b72bb798240c138a
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2022-07-20 19:49:59 +0800

    test fix

--- src/main/java/models/TourModel.java
+++ src/main/java/models/TourModel.java
@@ -28,38 +28,6 @@ import java.util.ArrayList;
 
 public class TourModel {
 
-    public static IDatabaseQuery<Tour> getUniqueToursByLowestPriceFirst() {
-        return databaseConnection -> {
-            Connection conn = databaseConnection.get();
-            try {
-                PreparedStatement prepStatement = conn.prepareStatement("SELECT t.*, a.price, b.url, b.alt_text\r\n"
-                        + " FROM tour t"
-                        + " LEFT JOIN ("
-                        + " 	SELECT DISTINCT t.name, td.tour_id, td.price"
-                        + "     FROM tour_date td"
-                        + "     LEFT JOIN tour t ON td.tour_id = t.tour_id"
-                        + "     WHERE td.show_tour = 1"
-                        + "     ORDER BY td.price"
-                        + " ) a ON a.tour_id = t.tour_id"
-                        + " LEFT JOIN ("
-                        + " 	SELECT DISTINCT tti.tour_id, ti.alt_text, ti.url"
-                        + "     FROM tour_tour_image tti"
-                        + "     LEFT JOIN tour_image ti ON ti.tour_image_id = tti.tour_image_id"
-                        + " ) b  ON b.tour_id = t.tour_id LIMIT 5;");
-                ResultSet rs = prepStatement.executeQuery();
-
-                ArrayList<Tour> list = new ArrayList<>();
-
-                if (rs != null) while (rs.next()) list.add(new Tour(rs));
-
-                return list.toArray(new Tour[0]);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-        };
-    }
-
     public static IDatabaseQuery<Tour> getTourById(int tourId) {
         return databaseConnection -> {
             Connection conn = databaseConnection.get();
@@ -284,22 +252,10 @@ public class TourModel {
         return databaseConnection -> {
             Connection conn = databaseConnection.get();
             try {
-                PreparedStatement pstmt = conn.prepareStatement("SELECT t.*, a.price, b.url, b.alt_text\r\n"
-                        + "FROM tour t\r\n"
-                        + "LEFT JOIN (\r\n"
-                        + "	SELECT DISTINCT t.name, td.tour_id, td.price\r\n"
-                        + "    FROM tour_date td\r\n"
-                        + "    LEFT JOIN tour t ON td.tour_id = t.tour_id\r\n"
-                        + "    WHERE td.show_tour = 1\r\n"
-                        + "    ORDER BY td.price\r\n"
-                        + ") a ON a.tour_id = t.tour_id\r\n"
-                        + "LEFT JOIN (\r\n"
-                        + "	SELECT DISTINCT tti.tour_id, ti.alt_text, ti.url\r\n"
-                        + "    FROM tour_tour_image tti\r\n"
-                        + "    LEFT JOIN tour_image ti ON ti.tour_image_id = tti.tour_image_id\r\n"
-                        + ") b  ON b.tour_id = t.tour_id\r\n"
-                        + "WHERE t.name LIKE '%" + name + "%'\r\n"
-                        + ";");
+                PreparedStatement pstmt = conn.prepareStatement("SELECT t.*, a.price, b.url, b.alt_text "
+                        + "FROM tour "
+                        + "WHERE t.name "
+                        + "LIKE \'%" + name + "%'; ");
                 ResultSet rs = pstmt.executeQuery();
 
                 ArrayList<Tour> list = new ArrayList<>();
