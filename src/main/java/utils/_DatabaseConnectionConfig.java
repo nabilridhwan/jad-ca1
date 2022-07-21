962cd9717bb2c4bd5237d6605ffa4fed7cfff283
catcd1w3r5 <45095604+catcd1w3r5@users.noreply.github.com>
2022-07-21 15:27:24 +0800

    Create _DatabaseConnectionConfig.java

---
+++ src/main/java/utils/_DatabaseConnectionConfig.java
@@ -0,0 +1,20 @@
+/*
+_DatabaseConnectionConfig.java_DatabaseConnectionConfig.java * 	Name: Xavier Tay Cher Yew
+	Admin No: P2129512
+	Class: DIT/FT/2A/01
+	Group Number: Group 4 - TAY CHER YEW XAVIER, NABIL RIDHWANSHAH BIN ROSLI  
+ * */
+
+package utils;
+
+
+/*
+To use, please duplicate this file,
+fill in the following variables with the correct information
+and rename this file and class name to DatabaseConnectionConfig (without the _)
+ */
+public class _DatabaseConnectionConfig {
+    protected static final String databaseName = "";
+    protected static final String user = "";
+    protected static final String password = "";
+}
